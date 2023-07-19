ce05954c9dd6cf1cf6f757a847229d2a1130c60b
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-07-19 23:22:56 +0100

    multiple-system-crud

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiple crud system</title>
+    <link href="https://cdn.tailwindcss.com/2.2.19/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+    @include('layout.nav')
+    @yield('content')
+</body>
+</html>
