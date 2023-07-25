cc202308f4d2b198009814addbc1f754df42717b
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-07-25 17:21:45 +0100

    adding all-cruds-page

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.base')
 @section('content')
 
-<div class="mx-auto justify-center items-center p-5 m-5 flex flex-wrap gap-1">
+<div class="mx-auto justify-center items-center p-5 mt-5 flex flex-wrap gap-1">
     <!-- Students crud -->
     <a href="{{ url('/students') }}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Students Crud</h5>
